6fc104e647e6b864b710f615fcb9a1b8af84eac2
sacarvy <sukhpreetben10@gmail.com>
2021-06-14 15:54:29 +0530

    Home page ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
     mode: 'jit',
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        screens: {
+            'xss':'400px',
+            'xs':'500px',
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+      
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+      
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+      
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+      
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },      
+        extend: {
+            colors:{
+                ...colors
+            }
+        },
     },
     variants: {
         extend: {},
